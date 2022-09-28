cb02521953c9bee063c6a9f20bd50c9d9fa2f84f
Jeffrey <jeff.ho@hotmail.com>
2022-09-28 15:32:35 -0400

    Set up database connection

---
+++ back-end/db/seeds/users_seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (cart_id, name, phone_number, email_address)
+VALUES (1, 'John Smith', 4368541593, 'john@smith.com'),
+(2, 'Kenny Yanb', 6535255720, 'kenny@yanb.com'),
+(3, 'Sara Dar', 1259524833, 'sara@dar.com'),
+(4, 'Tony Jortons', 1259524833, 'tony@jortons.com');
