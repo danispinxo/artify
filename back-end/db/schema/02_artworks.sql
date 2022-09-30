446c07deafc6f732ed7776b914002a7fe641106f
Jeffrey <jeff.ho@hotmail.com>
2022-09-30 14:24:01 -0400

    Add artworks seed

--- back-end/db/schema/02_artworks.sql
+++ back-end/db/schema/02_artworks.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS artworks CASCADE;
 
 CREATE TABLE artworks (
   id SERIAL PRIMARY KEY NOT NULL, 
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE, 
   name VARCHAR(255), 
   price_cents INTEGER, 
