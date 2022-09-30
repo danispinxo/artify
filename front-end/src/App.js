d90d0cbc4ec6cb29fb20890616c5aade0a03c4a7
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-09-30 15:35:30 -0400

    Setup react-router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,56 @@
-import './App.scss';
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import "./App.scss";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([{}]);
 
   useEffect(() => {
-    axios.get("/api")
-    .then(res => (res.data))
-    .then(data => setBackendData(data))
-  }, [])
-  
- console.log(backendData)
+    axios
+      .get("/api")
+      .then((res) => res.data)
+      .then((data) => setBackendData(data));
+  }, []);
+
+  console.log(backendData);
 
   return (
-    <div className="App"> 
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+
       <h1>Contact List</h1>
       <div className="user-list">
         <ul className="list">
-          {(backendData.length > 0) && backendData.map((user, i) => <li key={i}><img src={user.avatar} alt="avatar" width="100px"/> {user.name} {user.surname}: {user.email} </li>)}
+          {backendData.length > 0 &&
+            backendData.map((user, i) => (
+              <li key={i}>
+                <img src={user.avatar} alt="avatar" width="100px" /> {user.name}{" "}
+                {user.surname}: {user.email}{" "}
+              </li>
+            ))}
         </ul>
-      </div>   
+      </div>
     </div>
   );
 }
