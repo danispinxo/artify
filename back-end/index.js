fc0d4f2955399670e7f6599dc29ddca0baa00335
danispinxo <genericpronoun@gmail.com>
2022-09-30 19:06:03 +0000

    fixing queries

--- back-end/index.js
+++ back-end/index.js
@@ -6,27 +6,20 @@ const morgan = require("morgan");
 const port = 8080;
 
 // Middleware
-
 app.use(express.urlencoded({ extended: true }));
-
 
 // Separated Routes for each Resource
 //const widgetApiRoutes = require('./routes/widgets-api');
 const testRoute = require("./routes/users")
 
-
 // Mount all resource routes
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use("/api", testRoute)
-
-
 
 //Home page
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
