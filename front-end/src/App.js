3b85c7594248c03d5e30867576a7f202e5aa73d4
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-01 15:39:18 -0400

    update navbar nad refactor app.js file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,20 @@
 import "./App.scss";
 import React from "react";
-import { Navbar } from "./components/Navbar";
-
+import { Home } from "./Pages/Home";
+import { Header } from "./components/Header";
+import { Gallery } from "./Pages/Gallery";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App"> 
-      <Navbar />
-   
-         
-
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../styles/navbar.scss';
 
 export const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="p-3">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+    <Navbar className="navbar"  expand="lg">
+      <Container fluid>
+        <Navbar.Brand>
+          <Link className="text-decoration-none text-black" to="/">
+            Artify
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
             <Nav.Link>
-              {' '}
-              <Link className="text-decoration-none text-white" to="/">
+              <Link className="text-decoration-none text-black" to="/">
                 Home
               </Link>
             </Nav.Link>
             <Nav.Link>
-              {' '}
-              <Link className="text-decoration-none text-white" to="/gallery">
+              <Link className="text-decoration-none text-black" to="/gallery">
                 Gallery
               </Link>
             </Nav.Link>
-            
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
           </Nav>
-          {/* <Nav className="gap-2">
-            <Nav.Link className="btn btn-primary" href="#">Login</Nav.Link>
-            <Nav.Link eventKey={2} className="btn btn-light text-black" href="#">
-              Sign up
-            </Nav.Link>
-          </Nav> */}
+         
         </Navbar.Collapse>
       </Container>
     </Navbar>
-)
-}
+  );
+};
