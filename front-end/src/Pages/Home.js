e1d0f149342441421eb6653b7abad6804d69346d
Jeffrey <jeff.ho@hotmail.com>
2022-10-01 16:06:51 -0400

    Fix carousel bug. Implement category tabs in homepage

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -5,11 +5,13 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "../styles/homepage.scss";
-
 import { Pagination, Navigation } from "swiper";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 export const Home = () => {
   const [images, setImages] = useState([]);
+  const [key, setKey] = useState('home');
 
   useEffect(() => {
     axios
@@ -20,11 +22,12 @@ export const Home = () => {
 
   return (
     <>
+    <h1>LETS BUY ART!</h1>
       {images.length > 0 && (
         <Swiper
         slidesPerView={4}
-        spaceBetween={10}
-        slidesPerGroup={3}
+        spaceBetween={30}
+        slidesPerGroup={4}
         loop={true}
         loopFillGroupWithBlank={true}
         pagination={{
@@ -40,6 +43,24 @@ export const Home = () => {
             </SwiperSlide>
           ))}
         </Swiper>
+      )}
+      {images.length > 0 && (
+        <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+    
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          
+        </Tab>
+        <Tab eventKey="contact" title="Contact">
+  
+        </Tab>
+      </Tabs>
       )}
     </>
   );
