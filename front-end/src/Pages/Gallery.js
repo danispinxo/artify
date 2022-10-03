b66c78a149ce995a295e51e6e44c332880c2c397
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-03 14:31:07 -0400

    Updating Gallery and Adding some css

--- front-end/src/Pages/Gallery.js
+++ front-end/src/Pages/Gallery.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import Image from 'react-bootstrap/Image';
+import "../styles/gallery.scss";
 
 export const Gallery = () => {
   const {id} = useParams();
@@ -40,6 +41,7 @@ export const Gallery = () => {
             <p>"{artwork.name}" -- Price $ {artwork.price_cents / 100.00}</p>
           </div>)}
       </div>
+     
     </div>
   )
 }
