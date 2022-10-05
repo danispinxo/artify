3c9e38612eb9b9ba2d6210242638c8106d2682ac
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-05 00:06:25 -0400

    Refactor Category Item component

--- front-end/src/components/CategoryItem.js
+++ front-end/src/components/CategoryItem.js
@@ -1,20 +1,22 @@
-import Button from "./Button";
 import "../styles/categoryitem.scss";
 import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 
 export default function CategoryItem(props) {
   const { name, image, id } = props;
 
   return (
     <div className="categoryitem-container">
-      <div className="category-image">
-        <img src={image} alt={image} />
-      </div>
-      <div className="category-button">
-        <Link to={`/category/${id}`}>
-          <Button message={name} />
-        </Link>
-      </div>
-    </div>
+      <Link className="card5" to={`/category/${id}`}>
+      <Card className="category-body">
+        <Card.Img variant="top" src={image} alt={image} className="card-image" />
+        <Card.Body >
+          <Card.Title className="card-title2">
+            <h5>{name}</h5>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+      </Link>
+  </div>
   );
 }
