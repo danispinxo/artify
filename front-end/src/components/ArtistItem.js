09b4cdcad3bc96cc3c1e6c5e7180e602b87006a0
danispinxo <genericpronoun@gmail.com>
2022-10-05 13:28:00 +0000

    created artistItem component and adjusted styling

--- front-end/src/components/ArtistItem.js
+++ front-end/src/components/ArtistItem.js
@@ -1,4 +1,4 @@
-import "../styles/categoryitem.scss";
+import "../styles/artistitem.scss";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -9,15 +9,17 @@ export default function ArtistItem(props) {
   const { name, image, id, sample_art } = props;
 
   return (
-    <div className="categoryitem-container">
-      <Link className="card5" to={`/gallery/${id}`}>
+    <div className="artistitem-container">
+      <Link className="card" to={`/gallery/${id}`}>
       <Card className="category-body">
         <Card.Img variant="top" src={sample_art} alt={image} className="card-image" />
         <Card.Body >
-          <Card.Title className="card-title2">
+          <Card.Title className="card-title">
             <Image src={image} alt={name + "'s Avatar"} roundedCircle="true" width="75px" />
-            <h5>{name}</h5>
-            <FontAwesomeIcon icon={faEnvelope} />
+            <div className="contact">
+              <h5>{name}</h5>
+              <FontAwesomeIcon icon={faEnvelope} />              
+            </div>
           </Card.Title>
         </Card.Body>
       </Card>
