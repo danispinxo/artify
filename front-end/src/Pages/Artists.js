1f6b927fb4139d0bbb892bbd47c4962411da8460
danispinxo <genericpronoun@gmail.com>
2022-10-05 12:52:02 +0000

    adjusting nav bar and adding categories page

---
+++ front-end/src/Pages/Artists.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useContext } from "react";
+import "../styles/homepage.scss";
+import CategoryItem from "../components/CategoryItem";
+import { DataContext } from "../context/dataContext";
+
+export default function Artists() {
+  const dataState = useContext(DataContext);
+
+  useEffect(() => {
+    if (dataState.artworks) {
+      dataState.setArtworks(dataState.artworks);
+      dataState.setCategories(dataState.categories);
+    }
+  }, [dataState]);
+
+  return (
+    <div className="homepage-container">
+
+      <h1>Explore All the Artify Artists</h1>
+      This will be a list of all the artists.
+      {/* <div className="categories-container">
+        {dataState.categories.map((category, i) => (
+          <CategoryItem
+            key={i}
+            id={category.id}
+            name={category.name}
+            image={category.image}
+          />
+        ))}
+      </div> */}
+    </div>
+  );
+};
---
+++ front-end/src/Pages/Categories.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useContext } from "react";
+import "../styles/homepage.scss";
+import CategoryItem from "../components/CategoryItem";
+import { DataContext } from "../context/dataContext";
+
+export default function Categories() {
+  const dataState = useContext(DataContext);
+
+  useEffect(() => {
+    if (dataState.artworks) {
+      dataState.setArtworks(dataState.artworks);
+      dataState.setCategories(dataState.categories);
+    }
+  }, [dataState]);
+
+  return (
+    <div className="homepage-container">
+
+      <h1>Explore All the Artify Categories</h1>
+      <div className="categories-container">
+        {dataState.categories.map((category, i) => (
+          <CategoryItem
+            key={i}
+            id={category.id}
+            name={category.name}
+            image={category.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
