a0295d1e3c69430e481bd45974081db8e7c8f215
danispinxo <genericpronoun@gmail.com>
2022-10-06 17:36:16 +0000

    adjusting seeds for messages

--- back-end/db/schema/06_commissions.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS commissions CASCADE;
-
-CREATE TABLE commissions ( 
-  id SERIAL PRIMARY KEY NOT NULL, 
-  customer_id INTEGER REFERENCES users(id), 
-  artist_id INTEGER REFERENCES users(id), 
-  complete BOOLEAN, 
-  description TEXT
-);
