3d2ff34d08fc77dd0f579f1a05660be246b4e043
danispinxo <genericpronoun@gmail.com>
2022-10-07 18:19:25 +0000

    formatting line items

--- front-end/src/Pages/Confirmation.js
+++ front-end/src/Pages/Confirmation.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import '../styles/confirmation.scss';
-import Table from 'react-bootstrap/Table';
 import ConfirmationLineItem from '../components/ConfirmationLineItem';
 import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -30,13 +29,9 @@ export default function Confirmation() {
       <h1 className="success-confirmation">Success {firstName} {lastName}, your order has been placed!</h1>
       <h3>Order No. {orderID}</h3>
       <FontAwesomeIcon icon={faCircleCheck} className="confirm-logo" />
-      <table>
-        <tbody>
-          {order.length > 0 && order.map((item, index) => (
+      {order.length > 0 && order.map((item, index) => (
             <ConfirmationLineItem index={index} image={item.image} name={item.name} price_cents={item.price_cents} />
-          ))}
-        </tbody>
-      </table>
+      ))}
       <h3>You will also receive a confirmation email outlining your order details</h3>
       
       <button className="back-to-home" onClick={()=> navigate('/')}>
--- front-end/src/components/ConfirmationLineItem.js
+++ front-end/src/components/ConfirmationLineItem.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import '../styles/confirmation.scss'
-import Image from 'react-bootstrap/Image';
 import { Currency } from 'react-tender';
 import { styled } from '@mui/material/styles';
 import Rating from '@mui/material/Rating';
@@ -16,14 +15,13 @@ export default function ConfirmationLineItem({index, image, name, price_cents}) 
   });
 
   return (
-    <div className="confirmation-line-item">
-      <tr className='line-item' key={index}>
-        <td><Image src={image} alt={name} width="50px" /></td>
-        <td>{name}</td>
-        <td><Currency value={price_cents /100} currency="CAD" /></td>             
-        <td><StyledRating name="simple-controlled" value={rating} onChange={(event, newValue) => {
-        setRating(newValue)}} /></td>
-      </tr>
+    <div className="confirmation-line-item" key={index}>
+      <img className="line-item-image" src={image} alt={name}></img>
+      <div className="line-item-info">
+        <h4>{name}</h4>
+        <p><Currency value={price_cents / 100} currency="CAD" /></p>
+          <StyledRating name="simple-controlled" value={rating} onChange={(event, newValue) => {setRating(newValue)}} />
+      </div>
     </div>
   )
 }
