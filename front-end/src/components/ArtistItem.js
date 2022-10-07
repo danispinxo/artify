e5ba3a8a04c2b6e41e10a46186c3787301bc40f4
danispinxo <genericpronoun@gmail.com>
2022-10-07 14:58:16 +0000

    removing your from nav bar and rem envelope on artist page

--- front-end/src/components/ArtistItem.js
+++ front-end/src/components/ArtistItem.js
@@ -1,7 +1,5 @@
 import "../styles/artistitem.scss";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Card from "react-bootstrap/Card";
 import Image from 'react-bootstrap/Image';
 import "../styles/artistitem.scss";
@@ -16,13 +14,8 @@ export default function ArtistItem(props) {
         <Card.Img variant="top" src={sample_art} alt={image} className="artistitem-card-image" />
         <Card.Body className="artistitem-card-body">
           <Card.Title className="artistitem-card-title">
-            <div className="artistitem-card-title-av">
             <Image src={image} alt={name + "'s Avatar"} roundedCircle="true" className="artistitem-card-avatar-img"/>
               <h5>{name}</h5>
-            </div>
-            <div className="artistitem-contact">
-              <FontAwesomeIcon icon={faEnvelope} />              
-            </div>
           </Card.Title>
         </Card.Body>
       </Card>
