7c758c7ad1593ed37328851c8774ef26f2d5f44a
Jeffrey <jeff.ho@hotmail.com>
2022-10-08 10:36:58 -0400

    Add CSS styling to unauthorized cart page

--- front-end/src/Pages/Cart.js
+++ front-end/src/Pages/Cart.js
@@ -116,8 +116,8 @@ export default function Cart({cart, setCart}) {
       {!user.id &&
       <div className='unauthorized-cart'>
         <h1>Unauthorized Cart</h1>
-        <div className='message'>
-          <p>It looks like you're not logged in! Only registered and logged-in users can purchase artworks at Artify!</p>
+        <div className='unauthorized-cart-message'>
+          <p>It looks like you're not logged in! Register or log in to purchase artworks.</p>
         </div>
       </div>
       }
