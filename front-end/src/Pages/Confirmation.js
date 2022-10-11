7cb52639d01ebbfff405f1c7701e738ef2be96de
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-11 17:53:01 -0400

    changed font and some css stuff

--- front-end/src/Pages/Confirmation.js
+++ front-end/src/Pages/Confirmation.js
@@ -27,12 +27,12 @@ export default function Confirmation() {
   return (
     <div className="confirmation">
       <h1 className="success-confirmation">Success {firstName} {lastName}, your order has been placed!</h1>
+      <h4>You will also receive a confirmation email outlining your order details</h4>
       <h3>Order No. {orderID}</h3>
       <FontAwesomeIcon icon={faCircleCheck} className="confirm-logo" />
       {order.length > 0 && order.map((item, index) => (
             <ConfirmationLineItem key={index} image={item.image} name={item.name} price_cents={item.price_cents} artist={item.user_id} customer={item.customer_id} />
       ))}
-      <h3>You will also receive a confirmation email outlining your order details</h3>
       
       <button className="back-to-home" onClick={()=> navigate('/')}>
         Continue Shopping
